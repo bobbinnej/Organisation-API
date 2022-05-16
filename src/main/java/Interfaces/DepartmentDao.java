d4497a1f408c58c852dd1200c86b75351c3b85ff
jackie <bobbinnej@gmail.com>
2022-05-16 13:09:54 +0300

    Write the CRUD in DepartmentDao whose methods will be implemented in Sql2oDepartmentDao

--- src/main/java/Interfaces/DepartmentDao.java
+++ src/main/java/Interfaces/DepartmentDao.java
@@ -1,11 +1,30 @@
 package Interfaces;
 
 import models.Department;
+import models.DepartmentNews;
+import models.User;
+
+import java.util.List;
 
 public interface DepartmentDao {
     //CRUD
 
     //create
     void addDepartment(Department department);
+
+    //Read
+    List<Department> getAllDepartment();
+    List<User>getDepartmentUsersById(int id);
+
+    List<DepartmentNews>getDepartmentNewsById(int id);
+
+    //update
+    void updateDepartment(Department department, String name, String description);
+
+    //clear
+    void clearAllDepartments();
+
+    //find by id
+    Department findById(int id);
 
 }
