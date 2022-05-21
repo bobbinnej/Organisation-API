525a15c0827ad04f8b2b84df368f642adc2e276e
jackie <bobbinnej@gmail.com>
2022-05-21 20:22:22 +0300

    Add licence to my works

--- src/main/java/sql2o/Sql2oDepartmentDao.java
+++ src/main/java/sql2o/Sql2oDepartmentDao.java
@@ -1,5 +1,82 @@
 package sql2o;
 
-public class Sql2oDepartmentDao {
+import Interfaces.DepartmentDao;
+import models.Department;
+import models.DepartmentNews;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Sql2oDepartmentDao implements DepartmentDao {
+
+    private final Sql2oNewsDao newsDao;
+    private final Sql20UserDao userDao;
+    //sql2o connection object
+    private final Sql2o sql2o;
+
+    public Sql2oDepartmentDao(Sql2oNewsDao newsDao, Sql20UserDao userDao, Sql2o sql2o) {
+        this.newsDao = newsDao;
+        this.userDao = userDao;
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void addDepartment(Department department) {
+
+    }
+
+    //method to get all departments
+    @Override
+    public List<Department> getAllDepartment() {
+        String sql="SELECT *FROM departments";
+        try(Connection con= sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Department.class);
+
+        }
+
+    }
+
+    @Override
+    public List<User> getDepartmentUsersById(int id) {
+       return userDao.getAll().stream()
+               .filter(user -> user.getDepartment_id()==id)
+               .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<DepartmentNews> getDepartmentNewsById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateDepartment(Department department, String name, String description) {
+        String sql="UPDATE departments set(name, description, dpt_size)=(:name, :description, :dpt_size)";
+        try(Connection con= sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("description", description)
+                    .addParameter("dpt_size", dpt_size)
+
+        }
+
+    }
+
+    //clear all departments
+    @Override
+    public void clearAllDepartments() {
+        String sql="DELETE FROM departments";
+        try(Connection con=sql2o.open()){
+            con.createQuery(sql).executeUpdate();
+        }
+
+    }
+
+    @Override
+    public Department findById(int id) {
+        return null;
+    }
 }
